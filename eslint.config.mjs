8af3c77850fb3812171620e71cf34afee3e91582
Masud <ec20503@qmul.ac.uk>
2024-11-22 17:14:14 +0000

    Added comments

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,25 @@
+/************* First Iteration *************/
+// ts are also getted linted by eslint and js files are also getting linted by tseslint
+// giving wrong lint errors for app.ts and index.js file
+
+// import globals from "globals";
+// import pluginJs from "@eslint/js";
+// import tseslint from "typescript-eslint";
+
+
+// /** @type {import('eslint').Linter.Config[]} */
+// export default [
+//   {files: ["**/*.{js,mjs,cjs,ts}"]},
+//   {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+//   {languageOptions: { globals: globals.browser }},
+//   pluginJs.configs.recommended,
+//   ...tseslint.configs.recommended,
+// ];
+/************* First Iteration *************/
+
+/************* Second Iteration *************/
+// lints run but tseslint does not display any lint errors from my app.ts file
+
 // import globals from "globals";
 // import pluginJs from "@eslint/js";
 // import tseslint from "typescript-eslint";
@@ -27,14 +49,22 @@
 //     },
 //   },
 // ];
+/************* Second Iteration *************/
 
+/*******Third Iteration****/
 import globals from "globals";
 import eslint from "@eslint/js";
-import tseslintPlugin from "@typescript-eslint/eslint-plugin";
+import tseslint from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
 
-/*Remove the commented import below for possible fix*/
-//import tseslint from "typescript-eslint";
+/*Comment the above imports and uncomment the below for possible fix
+ However, still does not achieve the goal to have eslint lint js files and tseslint lint onlt ts files
+*/
+// import globals from "globals";
+// import eslint from "@eslint/js";
+// import tseslintPlugin from "@typescript-eslint/eslint-plugin";
+// import tsParser from "@typescript-eslint/parser";
+// import tseslint from "typescript-eslint";
 
 export default [
   // JavaScript files configuration
@@ -62,7 +92,7 @@ export default [
       parser: tsParser,
     },
     plugins: {
-      ts: tseslintPlugin,
+      ts: tseslint, // Rename this tseslintPlugin for possible fix
     },
     rules: {
       ...tseslint.configs.recommended.rules,
